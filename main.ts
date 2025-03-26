67cce04c27bc3f5c0bcfbfcb28f5ecbeb3895021
gabicodm10 <gabriel.croquet@etik.com>
2025-03-26 10:08:50 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(3)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(4)
+})
+basic.forever(function () {
+	
+})
